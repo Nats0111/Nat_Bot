11e1865d19c1d82596d4f668ffd6753bf581eba8
Nats0111 <70712695+Nats0111@users.noreply.github.com>
2020-09-04 00:32:38 +0800

    Join^Leave

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix= '[')
+
+@bot.event
+async def on_ready():
+   print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+   channel = bot.get_channel(751116126754439239)
+   await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+   channel = bot.get_channel(751116126754439239)
+   await channel.send(f'{member} left!')
+
+bot.run('NzUxMDgzNTU2NTk5NDk2NzA1.X1D7Eg.eC-SX9sCXL9LB7oxwXEwhAt1eUk')
