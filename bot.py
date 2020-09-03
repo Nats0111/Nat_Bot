4cde47aa05b054c35c02a1abb3cd6f60273d7f35
Nats0111 <70712695+Nats0111@users.noreply.github.com>
2020-09-04 00:41:17 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix= '[')
+bot = commands.Bot(command_prefix= '*')
 
 @bot.event
 async def on_ready():
@@ -16,5 +16,11 @@ async def on_member_join(member):
 async def on_member_remove(member):
    channel = bot.get_channel(751116126754439239)
    await channel.send(f'{member} left!')
+
+@bot.command()
+async def ping(ctx):
+   await ctx.send(f'{round(bot.latency*1000)} (ms)')
+ 
+
 
 bot.run('NzUxMDgzNTU2NTk5NDk2NzA1.X1D7Eg.eC-SX9sCXL9LB7oxwXEwhAt1eUk')
